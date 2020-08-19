2ecc5df94712bf5006ebddf25311fe28c776d2b8
Jacob Bare <jacob@limit0.io>
2020-08-19 10:16:04 -0500

    Define projectUsing directive

--- packages/graphql-directive-project/src/directives/definitions.js
+++ packages/graphql-directive-project/src/directives/definitions.js
@@ -2,7 +2,7 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql`
 
-directive @auth on FIELD_DEFINITION
 directive @project(field: String, needs: [String!] = []) on FIELD_DEFINITION
+directive @projectUsing(type: String!) on OBJECT
 
 `;
