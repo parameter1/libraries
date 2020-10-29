e7f4d89004caf572a578580322ec011a9df898da
Jacob Bare <jacob@limit0.io>
2020-10-29 08:15:32 -0500

    Create build-match-regex package

---
+++ packages/build-match-regex/README.md
@@ -0,0 +1,62 @@
+# Build Match Regex
+Builds regular expressions for matching parts of a phrase to a value. Useful for autocompleters, typeaheads, or quasi-search functions.
+
+## Installation
+```
+yarn add @parameter1/build-match-regex
+```
+
+## Usage
+```js
+const buildMatchRegex = require('@parameter1/build-match-regex');
+
+const phrase = 'quick brown fox';
+const regex = buildMatchRegex({
+  phrase,
+  // can be one of 'starts', 'ends', 'exact', or 'contains'
+  position: 'contains', // default
+  // can be either 'all' or 'any'
+  words: 'all', // default
+  insensitive: true, // default, set to false to force case-sensitivity
+});
+
+regex.match('brown fox'); // false
+regex.match('brown fox quick'); // true
+regex.match('Quickest Brownest Fox!'); // true
+```
+
+### Options
+
+Assuming a phrase of `quick brown fox`...
+
+Position `starts`:
+ - Words: `any`
+   - Where a value starts with `quick`, `brown`, or `fox`
+   - `/^quick|^brown|^fox/i`
+ - Words: `all`
+   - Where a value literally starts with `quick brown fox`
+   - `/^quick brown fox/i`
+
+Position `ends`:
+ - Words: `any`
+   - Where a value ends with `quick`, `brown`, or `fox`
+   - `/quick$|brown$|fox$/i`
+ - Words: `all`
+   - Where a value literally ends with `quick brown fox`
+   - `/quick brown fox$/i`
+
+Position: `exact`:
+ - Words: `any`
+   - Where a value exactly matches `quick`, `brown`, or `fox`
+   - `/^quick$|^brown$|^fox$/i`
+ - Words: `all`
+   - Where a value exactly matches `quick brown fox`
+   - `/^quick brown fox$/i`
+
+Position `contains`:
+ - Words: `any`
+   - Where a value contains any partial matches of `quick`, `brown`, or `fox`
+   - `/quick|brown|fox/i`
+ - Words: `all`
+   - Where a value contains all partial matches of `quick`, `brown`, and `fox`
+   - `/(?=.*quick)(?=.*brown)(?=.*fox)/i`
---
+++ packages/build-match-regex/src/index.js
@@ -0,0 +1,71 @@
+const escapeRegex = require('escape-string-regexp');
+
+/**
+ * Assuming a phrase of `quick brown fox`...
+ *
+ * Position `starts`:
+ *  - Match: `any`
+ *    - Where a value starts with `quick`, `brown`, or `fox`
+ *    - `/^quick|^brown|^fox/i`
+ *  - Match: `all`
+ *    - Where a value literally starts with `quick brown fox`
+ *    - `/^quick brown fox/i`
+ *
+ * Position `ends`:
+ *  - Match: `any`
+ *    - Where a value ends with `quick`, `brown`, or `fox`
+ *    - `/quick$|brown$|fox$/i`
+ *  - Match: `all`
+ *    - Where a value literally ends with `quick brown fox`
+ *    - `/quick brown fox$/i`
+ *
+ * Position: `exact`:
+ *  - Match: `any`
+ *    - Where a value exactly matches `quick`, `brown`, or `fox`
+ *    - `/^quick$|^brown$|^fox$/i`
+ *  - Match: `all`
+ *    - Where a value exactly matches `quick brown fox`
+ *    - `/^quick brown fox$/i`
+ *
+ * Position `contains`:
+ *  - Match: `any`
+ *    - Where a value contains any partial matches of `quick`, `brown`, or `fox`
+ *    - `/quick|brown|fox/i`
+ *  - Match: `all`
+ *    - Where a value contains all partial matches of `quick`, `brown`, and `fox`
+ *    - `/(?=.*quick)(?=.*brown)(?=.*fox)/i`
+ */
+module.exports = ({
+  phrase,
+  position,
+  words,
+  insensitive = true,
+} = {}) => {
+  const flags = insensitive ? 'i' : undefined;
+  const tokens = escapeRegex(phrase).replace(/\s\s+/g, ' ').split(' ').filter((v) => v);
+
+  if (position === 'starts') {
+    // /^quick|^brown|^fox/i
+    if (words === 'any') return new RegExp(`${tokens.map((t) => `^${t}`).join('|')}`, flags);
+    // /^quick brown fox/i
+    return new RegExp(`^${tokens.join(' ')}`, flags);
+  }
+  if (position === 'ends') {
+    // /quick$|brown$|fox$/i
+    if (words === 'any') return new RegExp(`${tokens.map((t) => `${t}$`).join('|')}`, flags);
+    // /quick brown fox$/i
+    return new RegExp(`${tokens.join(' ')}$`, flags);
+  }
+  if (position === 'exact') {
+    // /^quick$|^brown$|^fox$/i
+    if (words === 'any') return new RegExp(`${tokens.map((t) => `^${t}$`).join('|')}`, flags);
+    // /^quick brown fox$/i
+    return new RegExp(`^${tokens.join(' ')}$`, flags);
+  }
+
+  // Contains...
+  // /quick|brown|fox/i
+  if (words === 'any') return new RegExp(`${tokens.join('|')}`, flags);
+  // /(?=.*quick)(?=.*brown)(?=.*fox)/i
+  return new RegExp(`${tokens.map((t) => `(?=.*${t})`).join('')}`, flags);
+};
