c825b750a8b0500add8ce693f16053d933db983f
Jacob Bare <jacob@parameter1.com>
2021-11-18 07:12:53 -0600

    Create graphql package

---
+++ packages/graphql/src/index.js
@@ -0,0 +1,7 @@
+const extractFragmentData = require('./extract-fragment-data');
+const extractFragmentName = require('./extract-fragment-name');
+
+module.exports = {
+  extractFragmentData,
+  extractFragmentName,
+};
