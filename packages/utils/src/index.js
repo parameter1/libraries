3c60fec48bf9ca216d749b51b17ea3815fffc93a
Jacob Bare <jacob@parameter1.com>
2022-01-19 15:51:07 -0600

    Create `ucFirst` util

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -11,4 +11,5 @@ export { default as isObject } from './is-object.js';
 export { default as objectHasKeys } from './object-has-keys.js';
 export { default as parseNumber } from './parse-number.js';
 export { default as round } from './round.js';
+export { default as ucFirst } from './uc-first.js';
 export { default as wait } from './wait.js';
