c9826fd4a7b6e71c5334c49d2163b70c5ce64c78
Jacob Bare <jacob@parameter1.com>
2022-01-19 23:14:59 -0600

    Hoist mocha and chai; configure eslint

---
+++ packages/mongodb/.eslintrc.cjs
@@ -0,0 +1,7 @@
+const { resolve } = require('path');
+
+module.exports = {
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { packageDir: [resolve(__dirname, './'), resolve(__dirname, '../../')] }],
+  },
+};
